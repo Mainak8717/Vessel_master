6cda56ed819324ca43d40e44c24278fb03f26b57
mainak8717 <m4404415>
2017-09-21 17:48:07 +0530

    Updated

--- VesselManagement/ViewController/TripDetailsVC.cs
+++ VesselManagement/ViewController/TripDetailsVC.cs
@@ -19,41 +19,8 @@ namespace VesselManagement
 		{
 			base.ViewDidLoad();
 			setDetailValues(getTripDetails());
-		}
-		public TripDetails getTripDetails()
-		{
-			
-			 tripDetails = new TripDetails(model.TripId,"123456789", "MMSI001", "Mainak Choudhury", "31-12-2017","08-04-2018","17-Aug-2017 04:22 PM",
-			                               "ACTIVE","Cargo","16:23","12:50","02:55","01:45","12:45","16:26","12:50","16:25");
-			return tripDetails;
-		}
-		public void setDetailValues(TripDetails details)
-		{
-			IMO.Text = details.IMO;
-			MMSI.Text = details.MMSI;
-			MASTER.Text = details.Master;
-			STARTDATE.Text = details.StartDate;
-			ENDDATE.Text = details.EndDate;
-			ETACALC.Text = details.ETACalculated;
-			CURRENTSTATUS.Text = details.CurrentStatus;
-			VESSEL.Text = details.VesselType;
-			NT_Time.Text = details.NT;
-			GT_Time.Text = details.GT;
-			LOA_Time.Text = details.LOA;
-			LBP_Time.Text = details.LBP;
-			BREADTH_Time.Text = details.Breadth;
-			FWD_Time.Text = details.FwdDraft;
-			AFT_Time.Text = details.AftDraft;
-			MAXDRAFT_time.Text = details.MaxSummerDraft;
-			TRIPID.Text ="Trip Id:" + model.TripId;
-			SOURCE.Text ="From: " + model.Source;
-			DESTINATION.Text ="To: "+ model.Destination;
 
-		}
-		public override void ViewDidLayoutSubviews()
-		{
-			scrollView.ContentSize = new CoreGraphics.CGSize(View.Frame.Width, firstButton.Frame.Y + firstButton.Frame.Size.Height + 200);
-						switch (AppDelegate.typeOfUser)
+			switch (AppDelegate.typeOfUser)
 			{
 				case "Manager":
 					{ 
@@ -91,7 +58,8 @@ namespace VesselManagement
 						fourthButton.SetTitle("Manage Boats", UIControlState.Normal);
 
 						firstButton.TouchUpInside += (sender, e) => { 
-                             this.NavigationController.PushViewController(new ManageVC_1("Agency"),true);
+
+							 this.NavigationController.PushViewController(new ManageVC_1("Agency"),true);
 						};
 						secondButton.TouchUpInside += (sender, e) => { 
 							this.NavigationController.PushViewController(new ManageVC_2(),true);
@@ -113,7 +81,8 @@ namespace VesselManagement
 						fourthButton.Hidden = true;
 
 						firstButton.TouchUpInside += (sender, e) => { 
-                             this.NavigationController.PushViewController(new ManageVC_1("Agency"),true);
+
+							 this.NavigationController.PushViewController(new ManageVC_1("Agency"),true);
 						};
 					}
 					break;
@@ -127,6 +96,41 @@ namespace VesselManagement
 					}
 					break;
 			}
+		}
+		public TripDetails getTripDetails()
+		{
+			
+			 tripDetails = new TripDetails(model.TripId,"123456789", "MMSI001", "Mainak Choudhury", "31-12-2017","08-04-2018","17-Aug-2017 04:22 PM",
+			                               "ACTIVE","Cargo","16:23","12:50","02:55","01:45","12:45","16:26","12:50","16:25");
+			return tripDetails;
+		}
+		public void setDetailValues(TripDetails details)
+		{
+			IMO.Text = details.IMO;
+			MMSI.Text = details.MMSI;
+			MASTER.Text = details.Master;
+			STARTDATE.Text = details.StartDate;
+			ENDDATE.Text = details.EndDate;
+			ETACALC.Text = details.ETACalculated;
+			CURRENTSTATUS.Text = details.CurrentStatus;
+			VESSEL.Text = details.VesselType;
+			NT_Time.Text = details.NT;
+			GT_Time.Text = details.GT;
+			LOA_Time.Text = details.LOA;
+			LBP_Time.Text = details.LBP;
+			BREADTH_Time.Text = details.Breadth;
+			FWD_Time.Text = details.FwdDraft;
+			AFT_Time.Text = details.AftDraft;
+			MAXDRAFT_time.Text = details.MaxSummerDraft;
+			TRIPID.Text ="Trip Id:" + model.TripId;
+			SOURCE.Text ="From: " + model.Source;
+			DESTINATION.Text ="To: "+ model.Destination;
+
+		}
+		public override void ViewDidLayoutSubviews()
+		{
+			scrollView.ContentSize = new CoreGraphics.CGSize(View.Frame.Width, firstButton.Frame.Y + firstButton.Frame.Size.Height + 200);
+
 		}
 	}
 }
